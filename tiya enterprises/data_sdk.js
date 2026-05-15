df2b1ed002283e603b531588da0375f3997fc824
ritiksharma0224-hue <ritiksharma0224@gmail.com>
2026-05-15 17:54:40 +0530

    Delete tiya enterprises directory

--- tiya enterprises/data_sdk.js
+++
@@ -1,9 +0,0 @@
-// Mock data SDK for local Development
-console.log("data_sdk initialized for Tiya Enterprises website.");
-
-window.dataSdk = {
-    // any possible data-related methods
-    fetch: async function (url) {
-        return { success: true, url };
-    }
-};
